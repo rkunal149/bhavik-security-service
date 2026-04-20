f1a1143affb5793390b90eab1408093cf6bdc9c1
jonh wick <kunalraut58273@gmail.com>
2026-04-20 22:34:57 +0530

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+import path from "path";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* @ts-ignore - Turbopack root config for Next 16 */
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "hebbkx1anhila5yf.public.blob.vercel-storage.com",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
